2c48e0c3c67cde991a89307cb31aab3546099e2e
Chhagan Sinha <chhagan.sinha@credentinfotech.com>
2024-06-15 12:21:02 +0530

    alert postion chenges in edit button pages

--- RMS/Shared/Employee.cs
+++ RMS/Shared/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace RMS.Dto
         public int ? DesignationId { get; set; }
         public string EmployeeDepartment { get; set; } = string.Empty;
         public int ? DepartmentId { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string EmpId { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
